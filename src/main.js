3c910e934d43a2a7e2c2a151d2b2ed17e4acd979
MelodicTechno <617179025@qq.com>
2025-01-25 11:20:52 +0800

    分类查询

--- src/main.js
+++ src/main.js
@@ -7,6 +7,7 @@ import router from '@/router'
 import App from './App.vue'
 import { createPinia } from 'pinia'
 import { createPersistedState } from 'pinia-persistedstate-plugin'
+import locale from 'element-plus/dist/locale/zh-cn.js'
 
 const app = createApp(App)
 const pinia = createPinia()
@@ -14,5 +15,5 @@ const persist = createPersistedState()
 pinia.use(persist)
 app.use(pinia)
 app.use(router)
-app.use(ElementPlus);
+app.use(ElementPlus, {locale});
 app.mount('#app')
